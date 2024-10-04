1a4d05c194bda28ebc97c1b9073087d5b494e22a
Sumit Pandit <spandit@classy.org>
2024-10-04 06:44:37 +0530

    Initial commit

---
+++ src/Oct24/Lab001.java
@@ -0,0 +1,20 @@
+package Oct24;
+
+public class Lab001 {
+    public static void main(String[] args) {
+
+        // Table of 10 using the SOP statements
+
+        System.out.println("10 x 1 = 10");
+        System.out.println("10 x 1 = 20");
+        System.out.println("10 x 1 = 30");
+        System.out.println("10 x 1 = 40");
+        System.out.println("10 x 1 = 50");
+        System.out.println("10 x 1 = 60");
+        System.out.println("10 x 1 = 70");
+        System.out.println("10 x 1 = 80");
+        System.out.println("10 x 1 = 90");
+        System.out.println("10 x 1 = 100");
+
+    }
+}
