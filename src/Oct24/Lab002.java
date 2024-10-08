f71659feb541d0ac9ad4226104723c14090a036c
Sumit Pandit <spandit@classy.org>
2024-10-08 08:17:53 +0530

    update 08 oct.

--- src/Oct24/Lab002.java
+++ src/Oct24/Lab002.java
@@ -3,5 +3,13 @@ package Oct24;
 public class Lab002 {
     public static void main(String[] args) {
 
+        // Simple calculator using the SOP statements
+
+        System.out.println("10 + 2 = "+(10+2));
+        System.out.println("10 + 2 = "+(11-1));
+        System.out.println("10 + 2 = "+(10/2));
+        System.out.println("10 + 2 = "+(10*5));
+        System.out.println("10 + 2 = "+(20%100));
+
     }
 }
