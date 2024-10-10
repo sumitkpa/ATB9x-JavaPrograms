4917ff8a2bd8d2fa349c871809cc2e224765c50d
Sumit Pandit <spandit@classy.org>
2024-10-10 06:03:45 +0530

    update 10 oct.

---
+++ src/Oct24/Lab003.java
@@ -0,0 +1,29 @@
+package Oct24;
+
+public class Lab003 {
+    public static void main(String[] args) {
+
+        //Print the table using the printf() command
+        int t = 5;
+        System.out.printf("%d x %d = %d",t, 1, t*1);
+        System.out.println();
+        System.out.printf("%d x %d = %d",t, 2, t*2);
+        System.out.println();
+        System.out.printf("%d x %d = %d",t, 3, t*3);
+        System.out.println();
+        System.out.printf("%d x %d = %d",t, 4, t*4);
+        System.out.println();
+        System.out.printf("%d x %d = %d",t, 5, t*5);
+        System.out.println();
+        System.out.printf("%d x %d = %d",t, 6, t*6);
+        System.out.println();
+        System.out.printf("%d x %d = %d",t, 7, t*7);
+        System.out.println();
+        System.out.printf("%d x %d = %d",t, 8, t*8);
+        System.out.println();
+        System.out.printf("%d x %d = %d",t, 9, t*9);
+        System.out.println();
+        System.out.printf("%d x %d = %d",t, 10, t*10);
+
+    }
+}
