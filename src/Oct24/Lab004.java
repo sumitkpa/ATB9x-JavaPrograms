087554a1a055c8ff96074624152cf8ae67ea099e
Sumit Pandit <spandit@classy.org>
2024-10-19 19:58:27 +0530

    update 19 oct.

---
+++ src/Oct24/Lab004.java
@@ -0,0 +1,19 @@
+package Oct24;
+
+public class Lab004 {
+    public static void main(String[] args) {
+
+        //Escape sequence
+
+        System.out.println("This is Escape Sequence for program");
+
+        System.out.println("This is a backslash: \\");
+        System.out.println("This is a single quote: \'");
+        System.out.println("This is a double quote: \"");
+        System.out.println("This is a tab: \t space");
+        System.out.println("This is a back space:\btest");
+        System.out.println("This is a new line: \n -->");
+        System.out.println("This is a carriage return: \r test"); // Overwrites the current line
+
+    }
+}
