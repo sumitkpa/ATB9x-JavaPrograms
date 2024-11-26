19b2e98af85c278b4b8a63226639e3f3ac33dfcd
Sumit Pandit <spandit@classy.org>
2024-11-26 15:08:57 +0530

    update

--- src/Oct24/GenericJava.java
+++ src/Oct24/GenericJava.java
@@ -3,17 +3,15 @@ package Oct24;
 public class GenericJava {
 
 
-    public static <DT> DT gmethod (DT a) {
-
-        System.out.println(a);
-        return a;
-    }
-
     public static void main(String[] args) {
 
         gmethod(a:5);
-        gmethod(a:"Sumit");
 
+    }
 
+    public static <DT> DT gmethod (DT a) {
+
+        System.out.println(a);
+        return null;
     }
 }
